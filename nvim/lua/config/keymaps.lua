19d575e3cbafb7310147f05e0e1a036126038e20
Juan Villalta <juanfv2@gmail.com>
2023-04-16 15:20:08 -0600

    laravel

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,17 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
 local options = {
-  noremap = true,
-  silent = true,
+    noremap = true,
+    silent = true
 }
 
 vim.keymap.set("n", "<leader><esc>", ":nohlsearch<cr>", options)
 vim.keymap.set("n", "<leader>y", '"*y', options)
 vim.keymap.set("n", "<leader>p", '"*p', options)
-vim.keymap.set("n", "<leader>we", ":w<cr>", options)
--- vim.keymap.set("n", "<leader>q", ":q<cr>", options)
 vim.keymap.set("n", "ww¡", ":%y<cr>", options)
 vim.keymap.set("n", "ww'", "ggVGp", options)
 vim.keymap.set("n", "ww0", ":%d<cr>", options)
